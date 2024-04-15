c698345e810de73348f50d09a11919438fd5e225
winniewu550 <wnwu.winnie2016@gmail.com>
2024-04-15 19:36:58 -0400

    week5 homework

---
+++ week5/215.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        minHeap = []
+
+        for num in nums:
+            heapq.heappush(minHeap, num)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+
+        return minHeap[0]
