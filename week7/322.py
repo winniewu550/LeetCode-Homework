8fd2595864815ed683974677826feee55208b584
winniewu550 <wnwu.winnie2016@gmail.com>
2024-04-29 17:09:50 -0400

    upload week7 hw

---
+++ week7/322.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        # dp[i] := the minimum number Of coins to make up i
+        dp = [0] + [amount + 1] * amount
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return -1 if dp[amount] == amount + 1 else dp[amount]
